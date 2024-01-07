7028a8d71b27fd6abb0719d26eeca00555e69dfb
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2024-01-07 23:11:28 +0100

    Update MainActivity.kt

--- app/src/main/java/com/vidovicsystems/gasolinafinder/MainActivity.kt
+++ app/src/main/java/com/vidovicsystems/gasolinafinder/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.vidovicsystems.gasolinafinder
 
-import  android.os.Bundle
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.vidovicsystems.gasolinafinder.ui.theme.GasolinaFinderTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,33 +36,42 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun MainActivityContent() {
-    Box {
-        Column(
-            modifier = Modifier.padding(8.dp)
-        ) {
-            Text(text = "Hola Mundo!")
-        }
-
-        Row {
-
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GasolinaFinderTheme {
         MainActivityContent()
     }
+}
+
+//Modificadores
+@Composable
+fun MainActivityContent() {
+    Box {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
+                .padding(horizontal = 5.dp)
+        ) {
+            Bienvenida()
+        }
+    }
+}
+
+//Componentes
+@Composable
+fun Bienvenida() {
+    Text(
+        text = "Bienvenido a GasolinaFinder",
+        color = Color.Red,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 50.sp,
+        fontSize = 40.sp,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .background(Color.Black)
+            .padding(horizontal = 30.dp)
+            .fillMaxWidth()
+    )
 }
